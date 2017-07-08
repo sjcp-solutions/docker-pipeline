bd39fa29858f153e86a5fb51b789c04b1b8e4fcb
Steven Cooney <steven.cooney@sjcp-solutions.com>
2017-07-08 22:02:46 +0100

    Use a deploy script rather than inline comands

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x #echo on
+
+echo \{\"source_type\":\"Tag\",\"source_name\":\"\$TRAVIS_TAG\"\} > tag-template.json
+envsubst < ci-resources/tag-template.json > tag-body.json
+curl -X POST -H "Content-Type: application/json" -d "@./tag-body.json" "$RELEASE_URL"
